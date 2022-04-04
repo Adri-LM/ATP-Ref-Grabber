7a75f0784b6b2dfc095de0f3ccb8d36dea517e90
Adrien <adrien.savary@ext.leroymerlin.fr>
2022-04-04 19:37:45 +0200

    feat: add basic UI with sample table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { RefTableComponent } from './components/ref-table/ref-table.component';
+import { ResizableModule } from "./resizable/resizable.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RefTableComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatToolbarModule,
+    ResizableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
